e5cc143cbaeb3e6855726dcb4ddc7dee05f987e6
Namxu2 <nguyenthenam0401@gmail.com>
2024-07-28 21:28:37 +0700

    commit code

---
+++ src/sanPham/TheLoaiService.java
@@ -0,0 +1,11 @@
+package sanPham;
+
+import sanPham.TheLoai;
+
+import java.util.List;
+
+public interface TheLoaiService {
+    List<TheLoai> getTheLoai();
+
+    public boolean add(TheLoai tl);
+}
